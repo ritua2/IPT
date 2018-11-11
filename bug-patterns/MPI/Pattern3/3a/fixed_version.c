4ba570316135c5cd0573093ccbc733097d742b4b
Trung <trung@Macs-MacBook-Pro-2.local>
2018-11-11 11:44:07 -0500

    Update pattern

--- bug-patterns/MPI/Pattern3/3a/fixed_version.c
+++ bug-patterns/MPI/Pattern3/3a/fixed_version.c
@@ -8,33 +8,30 @@ int main()
   MPI_Init(NULL,NULL);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  int arr [10];
-
-  
-  // calculating the number of iterations that each process will execute
-  int range = 10/size;
-
-  // calculating the start and end value of the for loop for each process
-  int p_start_value = rank*range;
-  int p_end_value = p_start_value + range;
-
-  for (int i = p_start_value ; i < p_end_value; i++) {
-
-    // compute all elements at even indexes first
-    if (i < 5) {
-      arr[i*2] =2 ;
-    }
-
-    // compute all elements at odd indexes second
-    else {
-      arr[(i-5)*2+1] =3;
-    } 
-  }
-
-  // Gathering the elements from each process to get the final array
+  int arr [10]; 
   int rev [10];
-  MPI_Gather(&arr[rank*range],range, MPI_INT,rev, range,MPI_INT,0, MPI_COMM_WORLD);
-
-  MPI_Finalize();
+  int range = 10/size;
+  int start = rank*range; int end = start + range; int sendbuff[range];
+  int counter = 0;
+  for (int i=start;i<end;i++) {
+    if (i < 5) {
+      arr[i*2] =2 ; sendbuff[counter++]=arr[i*2];
+    } 
+    else {
+      arr[(i-5)*2+1] =3; 
+      sendbuff[counter++]=arr[i*2];
+    }
+  }
+  MPI_Gather(sendbuff,range, MPI_INT,rev,range, MPI_INT,0, MPI_COMM_WORLD); 
+  for (int i=0;i<10;i++)
+  {
+    if (i < 5)
+    {
+      arr[i*2]=rev[i] ;
+    }
+    else
+  {  
+    arr[(i-5)*2+1]=rev[i];
+  }
   return 0;
 }
