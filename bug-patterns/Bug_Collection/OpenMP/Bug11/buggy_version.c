6f649c03c89666a6d0d0ae3b9db81f3605ce734f
Ritu Arora <rauta@tacc.utexas.edu>
2019-06-18 14:13:54 -0500

    Create buggy_version.c

---
+++ bug-patterns/Bug_Collection/OpenMP/Bug11/buggy_version.c
@@ -0,0 +1,22 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+  FILE *f;
+  int a[100];
+  f = fopen("test.txt","r");
+
+#pragma omp parallel default(none) shared(a, f)
+{
+
+  #pragma omp for
+  for (int i = 0; i < 100; i++) {
+    fscanf(f,"%d\n",&a[i]);
+    #pragma omp single
+    printf("%d\n",a[i]);
+  }
+}
+fclose(f);
+return 0;
+}
