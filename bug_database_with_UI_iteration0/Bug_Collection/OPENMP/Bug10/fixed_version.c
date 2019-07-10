72707b079ca8eeb924c6b12eb7a7eac0638d8ab7
Carlos Redondo <carlos.red@utexas.edu>
2019-07-09 19:03:56 -0500

    Updated OpenMP bugs

--- bug_database_with_UI_iteration0/Bug_Collection/OPENMP/Bug10/fixed_version.c
+++ bug_database_with_UI_iteration0/Bug_Collection/OPENMP/Bug10/fixed_version.c
@@ -2,25 +2,38 @@
 
 
 int main(){
-    int sum  = 0;
+    int sum =0;
+    int temp[100];
     int DATA_MAG = 100;
     int H[100];
     int scale_factor = 10;
-    #pragma omp parallel for reduction(+: sum)
-    for (int i =0; i < DATA_MAG;i++) {
-        H[i] = i;
-    }
+    int i;
     int LUT[100];
-    for (int i = 0; i < DATA_MAG; i++)
+
+    #pragma omp parallel default (none) private(i) shared (temp, LUT, H, scale_factor, sum, DATA_MAG)
+    {
+    #pragma omp for
+    for (i =0; i < DATA_MAG;i++) {
+        H[i] = i;
+        temp[i]=0;
+    }
+
+
+    #pragma omp single
+    for (i = 0; i < DATA_MAG; i++)
     {
         sum += H[i];
+        temp[i]=sum;
+    }
 
-        LUT[i] = sum * scale_factor;
+    #pragma omp for
+    for (i = 0; i < DATA_MAG; i++){
+        LUT[i] = temp[i] * scale_factor;
     }
-    for (int i = 0; i < 100; i++) {
-        printf("%d \n",LUT[i]);
+
     }
-	return 0;
+    for (i = 0; i < 100; i++) {
+        printf("%d  ",LUT[i]);
+    }
+        return 0;
 }
-
-
--- bug_database_with_UI_iteration0/Bug_Collection/OPENMP/Bug9/buggy_version.cpp
+++ bug_database_with_UI_iteration0/Bug_Collection/OPENMP/Bug9/buggy_version.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include<math.h>
+
 bool checkPrime(int n) {
     if (n == 2) return true;
-    if (n < 2 || n % 2 == 0) return false;      
+    if (n < 2 || n % 2 == 0) return false;
+
+          
 	#pragma omp parallel for
     for (int i = 3; i <= static_cast<int>(sqrt(n)); i += 2) {
         if (n % i == 0) return false;
