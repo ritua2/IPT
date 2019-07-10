1213ab028fa8816263497b73b8370006e5f16946
Carlos Redondo <carlos.red@utexas.edu>
2019-07-10 16:23:15 -0500

    Fixed MPI bug 5, error in passing pointer

--- bug_database_with_UI_iteration0/Bug_Collection/MPI/Bug5/fixed_version.c
+++ bug_database_with_UI_iteration0/Bug_Collection/MPI/Bug5/fixed_version.c
@@ -6,8 +6,8 @@ int main()
   int size;
   int rank;
   MPI_Init(NULL,NULL);
-  MPI_Comm_size(MPI_COMM_WORLD,size);
-  MPI_Comm_rank(MPI_COMM_WORLD,rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   int range = (20/size);
   int start = rank*range; 
   int end = start + range ; 
