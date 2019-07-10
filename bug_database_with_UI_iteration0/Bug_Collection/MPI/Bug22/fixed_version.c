ad6701197c59f956550a2da05b7ab516df1e5e92
Ritu Arora <rauta@tacc.utexas.edu>
2019-07-10 16:03:26 -0500

    Update fixed_version.c

--- bug_database_with_UI_iteration0/Bug_Collection/MPI/Bug22/fixed_version.c
+++ bug_database_with_UI_iteration0/Bug_Collection/MPI/Bug22/fixed_version.c
@@ -8,35 +8,50 @@ int main()
   MPI_Init(NULL,NULL);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  int arr [10]; 
-  int rev [10];
-  int range = 10/size;
-  int start = rank*range; int end = start + range; int sendbuff[range];
-  int counter = 0;
-  for (int i=start;i<end;i++) {
-    if (i < 5) {
-      arr[i*2] =2 ; sendbuff[counter++]=arr[i*2];
-    } 
+  int arr [100];
+
+  
+  // calculating the number of iterations that each process will execute
+  int range = 100/size;
+  int sendbuf[range];
+  int counter =0;
+  // calculating the start and end value of the for loop for each process
+  int p_start_value = rank*range;
+  int p_end_value = p_start_value + range;
+
+  for (int i = p_start_value ; i < p_end_value; i++) {
+
+    // compute all elements at even indexes first
+    if (i<50) {
+      arr[i*2] =2 ;
+      sendbuf[counter++]=arr[i*2];   
+    }
+
+    // compute all elements at odd indexes second
     else {
-      arr[(i-5)*2+1] =3; 
-      sendbuff[counter++]=arr[i*2];
+      arr[(i-50)*2+1] =3;
+      sendbuf[counter++]=arr[(i-50)*2+1];
     }
   }
-  MPI_Gather(sendbuff,range, MPI_INT,rev,range, MPI_INT,0, MPI_COMM_WORLD); 
-  for (int i=0;i<10;i++)
-  {
-    if (i < 5)
+
+  // Gathering the elements from each process to get the final array
+  int rev [100];
+  MPI_Gather(sendbuf,range, MPI_INT,rev, range,MPI_INT,0, MPI_COMM_WORLD);
+
+  for (int i=0;i<100;i++){
+    if (i < 50)
     {
-      arr[i*2]=rev[i] ;
+      	arr[i*2]=rev[i] ;
+    }else{  
+    	arr[(i-50)*2+1]=rev[i];
     }
-    else
-  {  
-    arr[(i-5)*2+1]=rev[i];
   }
-  return 0;
-}
-  if (rank == 0)
+
+  if (rank == 0){
     for (int i =0; i < 100; i++)
-      printf("a[%d] = %d\n",i,rev[i]);
+      printf("arr[%d] = %d\n",i,arr[i]);
+  
+  }
   MPI_Finalize();
+  return 0;
 }
