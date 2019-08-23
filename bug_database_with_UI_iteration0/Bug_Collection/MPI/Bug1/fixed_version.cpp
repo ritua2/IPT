97aa4dd643504177881d241e63739e7a60994600
Ritu Arora <rauta@tacc.utexas.edu>
2019-08-23 01:48:09 -0500

    Update fixed_version.cpp

--- bug_database_with_UI_iteration0/Bug_Collection/MPI/Bug1/fixed_version.cpp
+++ bug_database_with_UI_iteration0/Bug_Collection/MPI/Bug1/fixed_version.cpp
@@ -15,19 +15,29 @@ int main()
   int recvcounts [size];
   int displs [size];
   if (rank == size - 1) { 
-    end += 10 % size; 
-    range += 10 % size;
+    end += (10 % size); 
+    range += (10 % size);
   }
-  for (int i=0;i < size;i++) {
-    recvcounts[i] = range; 
-    displs[i]= (i==0)? 0: recvcounts[i-1]; 
+
+  recvcounts[0]=10/size;
+  displs[0]=0;
+  for (int i=1;i < size;i++) {
+    if(i<(size-1)){
+    	recvcounts[i] = 10/size;
+    }else if (i== (size-1)){
+	recvcounts[i]=10/size + (10%size);
+    }
+    displs[i]= displs[i-1]+ recvcounts[i-1];
+    printf("\ni: %d, rank: %d, displs[i]: %d, recvcounts[i]: %d\n",i, rank, displs[i], recvcounts[i]);
   }
   int recvbuf[range];
+
   for (int i=0;i < range;i++) {
-    recvbuf[i] = i; 
+    recvbuf[i] = i;
+    printf("\nrank:%d, i:%d, recvbuf[i]:%d\n", rank, i, recvbuf[i]); 
   }
-  MPI_Gatherv(recvbuf,range, MPI_INT,arr,displs,
-  recvcounts, MPI_INT, 0,MPI_COMM_WORLD);
+
+  MPI_Gatherv(recvbuf,range, MPI_INT,arr,recvcounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
   
   if (rank == 0) {
     for (int i = 0 ; i < 10 ; i++) {
