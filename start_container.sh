341963f78a8bcbef7f5342d307b494981569cb19
Carlos Redondo <carlos.red@utexas.edu>
2020-03-14 21:37:45 -0500

    Updated IPT CUDA userguide with instructions for the molecular dynamics program

--- start_container.sh
+++
@@ -1,2 +0,0 @@
-docker run --env="DISPLAY" -e mysql_database=ipt_bugs -e mysql_password="samplepassword"  --net=host --volume="/root/.Xauthority:/root/.Xauthority:ro" --name=krishan  -it --rm carlosred/ipt_bugs-database:latest bash
-
